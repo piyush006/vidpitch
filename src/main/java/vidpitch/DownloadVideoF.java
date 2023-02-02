0e047d23340beecc62b90069ade8408627c0ecbd
piyush <piyushsoni06@gmail.com>
2023-02-02 15:32:15 +0530

    First commmit

--- src/main/java/vidpitch/DownloadVideoF.java
+++ src/main/java/vidpitch/DownloadVideoF.java
@@ -1,16 +1,25 @@
 package vidpitch;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-public class DownloadVideoF extends Setup{
+public class DownloadVideoF {
 	
 	
 	@Test
-	public void DownloadVideo() throws InterruptedException {	
-	 driver.navigate().to("https://www.mailinator.com/");
+	public void DownloadVideo() throws InterruptedException {
+		System.setProperty("webdriver.firefox.marionette", "C:\\software\\geckodriver-v0.32.0-win-aarch64\\geckodriver.exe");
+      WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+	 driver.get("https://www.mailinator.com/");
 		
 	    driver.findElement(By.id("search")).sendKeys("Sarah.Bolding.b773cec85422409d97228934a4eb0d6b@mailinator.com");
 		Thread.sleep(3000);
